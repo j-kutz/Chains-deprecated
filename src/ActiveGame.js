0345f12b2ecc17f10c9e09ca236e08745cd234f3
Jordan Kutz <jkutz@go.olemiss.edu>
2019-10-06 01:55:11 -0500

    Got tabs working again

--- src/ActiveGame.js
+++ src/ActiveGame.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
 import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
+import { TabView, SceneMap } from 'react-native-tab-view';
+import produce from 'immer'
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
 
 export default class ActiveGame extends Component {
     static navigationOptions = {
@@ -16,52 +26,92 @@ export default class ActiveGame extends Component {
           fontWeight: 'bold',
         },
       };
+    
+    state = {
+      numOfHoles: 9,
+      index: 0,
+      routes: [],
+      sceneMap: {},
+    };
+
+    componentDidMount() {
+      let routesPlaceholder = [];
+      let sceneMapPlaceholder = {};
+      for(let i=1; i<=this.state.numOfHoles; i++){
+        routesPlaceholder.push({ key: i, title: 'Hole '+i });
+        sceneMapPlaceholder[i] = FirstRoute
+      }
+
+      this.setState({
+        routes: routesPlaceholder,
+        sceneMap: sceneMapPlaceholder,
+      })
+    }
 
     render() {
+      if(this.state.sceneMap && this.state.routes){
         return (
-            <TabNavigation />
+          <TabView
+            navigationState={this.state}
+            renderScene={SceneMap(this.state.sceneMap)}
+            onIndexChange={index => this.setState({ index })}
+            initialLayout={{ width: 100 }}
+          />
         )
+      } else {
+        return (
+          <View>
+            <Text>Loading...</Text>
+          </View>
+        )
+      }
     }
 }
 
-class Hole1 extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Hole 1!</Text>
-        </View>
-      );
-    }
-  }
-  
-  class Hole2 extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Hole 2!</Text>
-        </View>
-      );
-    }
-  }
-  
-  const TabNavigator = createMaterialTopTabNavigator({
-    'Hole 1': Hole1,
-    'Hole 2': Hole2,
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
   },
-  {
-      swipeEnabled: true,
-      tabBarOptions: {
-        labelStyle: {
-          fontSize: 12,
-        },
-        tabStyle: {
-          width: 100,
-        },
-        style: {
-          backgroundColor: '#6200ee',
-        },
-      }
-  }
-  );
+});
+
+// class Hole1 extends React.Component {
+//     render() {
+//       return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//           <Text>Hole 1!</Text>
+//         </View>
+//       );
+//     }
+//   }
   
-  const TabNavigation = createAppContainer(TabNavigator);
+//   class Hole2 extends React.Component {
+//     render() {
+//       return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//           <Text>Hole 2!</Text>
+//         </View>
+//       );
+//     }
+//   }
+  
+//   const TabNavigator = createMaterialTopTabNavigator({
+//     'Hole 1': Hole1,
+//     'Hole 2': Hole2,
+//   },
+//   {
+//       swipeEnabled: true,
+//       tabBarOptions: {
+//         labelStyle: {
+//           fontSize: 12,
+//         },
+//         tabStyle: {
+//           width: 100,
+//         },
+//         style: {
+//           backgroundColor: '#6200ee',
+//         },
+//       }
+//   }
+//   );
+  
+//   const TabNavigation = createAppContainer(TabNavigator);
